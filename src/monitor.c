ec9b8b2926790a915aefa2534be7bf8b0c7fe743
Jose Oliveira <zeeduardo2013@gmail.com>
2023-04-28 00:41:42 +0100

    Execute -u quase feito

--- src/monitor.c
+++ src/monitor.c
@@ -4,18 +4,20 @@
 #include <sys/wait.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 #include "/home/wally/SO_aulas/SO/include/monitor.h"
 
 int main(int args, char* argv[]){
     if(mkfifo("./obj/fifo",0666) < 0){
         printf("Fifo already exists\n");
     };
-    int fd = open("./obj/fifo", O_RDONLY);
+    int fifo = open("./obj/fifo", O_RDWR);
+    int size = 10;
+    processo* arr_procs = malloc(sizeof(processo)*10);
     while(1){
         processo name;
-        read(fd,&name,sizeof(processo));
-        if(name.processo == 0){
-            
+        if(read(fifo,&name,sizeof(int))>0){
+
         }
     }
 }
--- src/tracer.c
+++ src/tracer.c
@@ -9,10 +9,18 @@
 #include "/home/wally/SO_aulas/SO/include/tracer.h"
 
 int main(int args, char* argv[]){
-    int fd = open("./obj/fifo", O_WRONLY);
+    int fifo = open("./obj/fifo", O_WRONLY);
+    int fd[2];
+    processo name;
+    struct timeval tv;
     char** arg = &argv[3]; 
+
+    if(pipe(fd) < 0){
+        return 2;
+    }
+
     if(strcmp("status",argv[1]) == 0){
-        read(fd)
+
     }
     else{
         int pid = fork();
@@ -20,10 +28,20 @@ int main(int args, char* argv[]){
             printf("Fork nao abriu");
             return 2;
         }
-
-        int time = gettimeofday(NULL,NULL);
         if (pid == 0){
-            execvp(argv[2],arg);
+            close(fd[0]);
+            int x = getpid();
+            name.programa = argv[3];
+            name.processo = x;
+            name.timestamp = gettimeofday(&tv,NULL);
+            write(fd[1],&name,sizeof(processo));
+            execvp(argv[3],arg);
+        }
+        else{
+            close(fd[1]);
+            read(fd[0],&name,sizeof(processo));
+            write(fifo,&name,sizeof(processo));
+            close(fd[1]);
         }
     }
 }
