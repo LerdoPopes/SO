867fce6bb186dced87ae5d44edff5a4984a7041f
Jose Oliveira <zeeduardo2013@gmail.com>
2023-05-08 00:22:55 +0100

    quase feita -p

--- src/tracer.c
+++ src/tracer.c
@@ -9,13 +9,109 @@
 #include <stdlib.h>
 #include "/home/wally/SO_aulas/SO/include/tracer.h"
 
+char** parse(char* argv){
+    char** answer = malloc(sizeof(char*)*20);
+    int n = 0;
+    char* tok;
+    while((tok = strtok_r(argv, " ", &argv)) != NULL){
+        answer[n] = tok;
+        n++;
+    }
+    answer[n] = NULL;
+    return answer;
+}
+
 int main(int args, char* argv[]){
     int fifo = open("./obj/fifo", O_WRONLY);
     processo name;
     struct timeval tv;
-    char** arg = &argv[3];
+    char** arg;
+    if(args > 2){
+        arg = parse(argv[3]);
+        name.processo = getpid();
+        if((strcmp("execute",argv[1]) == 0) && (argv[2][1] == 'p')){
+            int n = 0;
+            int start, final = 0;
+            int current = 1,np = 1;
+            strcpy(name.programa, arg[0]);
+            for(int i = 0; arg[i];i++,final++){
+                if(arg[i][0] == '|'){
+                    strcat(name.programa, " | \0");
+                    strcat(name.programa, arg[i+1]);
+                    np++;
+                }
+            }
+            int fd[np][2];
+            for(int i = 0; i<np;i++){
+                pipe(fd[i]);
+            }
+            while(n<=final){
+                start = n;
+                while(n < final && arg[n][0] != '|'){
+                    n++;
+                }
+                int pid1 = fork();
+                if(pid1 == 0){
+                    char** arguments = malloc(sizeof(char*)*n);
+                    for(int i = start;i < n;i++){
+                        arguments[i] = arg[i];
+                    }
+                    if(start != 0){
+                        dup2(fd[current-2][0],0);
+                        close(fd[current-2][0]);
+                    }
+                    else{
+                        write(fifo,&name,sizeof(processo));
+                    }
+                    if(n < final){
+                        dup2(fd[current-1][1],1);
+                        close(fd[current-1][1]);
+                    }
+                    execvp(arguments[start], arguments);
+                    _exit(0);
+                }
+                close(fd[current-1][1]);
+                close(fd[current-1][0]);
+                wait(NULL);
+                current++;
+                n++;
+            }
+            close(fd[current][0]);
+            close(fd[current][1]);
+            // name.processo = getpid();
+            // name.programa[0] = 0;
+            // gettimeofday(&tv,NULL);
+            // name.tv = tv;
+            // write(fifo,&name,sizeof(processo));
+        }
 
-    if(strcmp("status",argv[1]) == 0){
+        else if(strcmp("execute",argv[1]) == 0){
+            int pid = fork();
+            if(pid == -1){
+                printf("Fork nao abriu");
+                return 2;
+            }
+            if (pid == 0){
+                int x = getppid();
+                strcpy(name.programa,arg[0]);
+                name.processo = x;
+                gettimeofday(&tv,NULL);
+                name.tv = tv;
+                write(fifo,&name,sizeof(processo));
+                execvp(arg[0],arg);
+            }
+            else{
+                wait(NULL);
+                // name.processo = getpid();
+                // name.programa[0] = 0;
+                // gettimeofday(&tv,NULL);
+                // name.tv = tv;
+                // write(fifo,&name,sizeof(processo));
+                // close(fifo);
+            }
+        }
+    }
+    else if(strcmp("status",argv[1]) == 0){
         name.processo = 0;
         write(fifo, &name, sizeof(processo));
         mkfifo("./obj/status", 0666); 
@@ -27,30 +123,12 @@ int main(int args, char* argv[]){
             write(1,str,strlen(str));
         }
         close(stats);
+        return 0;
     }
-    else if(strcmp("execute",argv[1]) == 0){
-        int pid = fork();
-        if(pid == -1){
-            printf("Fork nao abriu");
-            return 2;
-        }
-        if (pid == 0){
-            int x = getpid();
-            strcpy(name.programa,argv[3]);
-            name.processo = x;
-            gettimeofday(&tv,NULL);
-            name.tv = tv;
-            write(fifo,&name,sizeof(processo));
-            execvp(argv[3],arg);
-        }
-        else{
-            wait(NULL);
-            name.processo = pid;
-            name.programa[0] = 0;
-            gettimeofday(&tv,NULL);;
-            name.tv = tv;
-            write(fifo,&name,sizeof(processo));
-            close(fifo);
-        }
-    }
+    name.processo = getpid();
+    name.programa[0] = 0;
+    gettimeofday(&tv,NULL);
+    name.tv = tv;
+    write(fifo,&name,sizeof(processo));
+    close(fifo);
 }
